3556ca0c82077fcf743534e153529f691ec1cbd7
aguinaldojunior31 <117935105+aguinaldojunior31@users.noreply.github.com>
2023-09-04 20:48:15 -0300

    Add files via upload

---
+++ Casa.java
@@ -0,0 +1,19 @@
+public class Casa  {
+
+        private String endereço;
+        private Especificação espec;
+        
+        public Casa(String Comodos, Especificação espec){
+            this.Comodos=Comodos;
+            this.espec=espec;
+        }
+        
+        public String getValor() {
+            return Valor;
+        }
+        public void setValor(string novaCasa) {
+        }
+        public Especificação getEspec() {}
+            // TODO Auto-generated method stub
+    
+}
---
+++ Especificação.java
@@ -0,0 +1,37 @@
+public class Especificação {
+
+    private String rua;
+    private String comodo;
+    private String bairro;
+
+    public Especificação(String rua, String comodo, String bairro) {
+        this.rua = rua;
+        this.comodo = comodo;
+        this.bairro = bairro;
+    }
+
+    public getRua(){
+        return rua;
+    }
+
+    public String setRua() {
+        return rua;
+    }
+
+    public getcomodo(){
+        return comodo;
+    }
+
+    public String setcomodo() {
+        return comodo;
+    }
+
+    public getbairro(){
+        return bairro;
+    }
+
+    public String bairro() {
+        return bairro;
+    }
+
+}

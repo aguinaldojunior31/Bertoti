78f5b01edf851d84173a5a95e54fb8a3a879ac96
aguinaldojunior31 <117935105+aguinaldojunior31@users.noreply.github.com>
2023-09-04 20:02:43 -0300

    Add files via upload

---
+++ public class Casa {.js
@@ -0,0 +1,20 @@
+public class Casa  {
+
+	private string endereço
+	private especificação espec
+	
+	public Casa(String Comodos, Especificação espec){
+		this.Comodos=Comodos;
+		this.espec=espec;
+	}
+	
+	public String get Valor() {
+		return valor
+	}
+	public void set Valor(String nova Casa) {
+	}
+	public especificação get Espec() {}
+		// TODO Auto-generated method stub
+
+	}
+
